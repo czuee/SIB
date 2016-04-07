bced765e2d18312ebafedb7715c15d816fd2b754
czuee <czuee.morey@gmail.com>
2016-04-07 14:10:10 +0200

    genes & sample clustering, 1st gene group same from diff methods

--- expression-mean0_HM.R
+++ expression-mean0_HM.R
@@ -121,7 +121,7 @@ ggplot(dat) + geom_boxplot(aes(x=variable, y=value, color=gender)) + coord_flip(
 
 #Which genes have a significant difference in their means? Abs Mean diff > SD of either M or F
 
-#Kurtosis calculation
+#Kurtosis calculation 
 install.packages("e1071")
 library(e1071)
 
@@ -132,6 +132,8 @@ g2k.kurt.f <- sapply(g2k.f[ ,-(1:2)], kurtosis)
 g2k.k[1250 ,]
 g2k.k <- data.frame(total=g2k.kurt, male=g2k.kurt.m, female=g2k.kurt.f)
 g2k.k[, 4] = (g2k.k$total > max(g2k.k$male, g2k.k$female))
+
+#The kurtosis might not be a good measure because of the outliers.
 
 g2k.f <- filter(g2k.mf, gender == "female")
 g2k.m <- filter(g2k.mf, gender == "male")
@@ -152,3 +154,83 @@ plot(meanM ~ meanF, mean.mf, col=(signif + 1), main = "Mean differences (16 sign
 Genes.signif <- rownames(mean.mf)[mean.mf$signif == TRUE]
 
 ### End Controls ###
+
+#Clustering
+#The dist function calculates distances between rows of a matrix. I'm using Euclidean distance here since the data is log tranformed.
+g1k.mf.m <- data.matrix(g1k.mf[ ,-(1:2)])
+g1k.dist.sam <- dist(g1k.mf.m)
+
+clust.sam <- hclust(g1k.dist.sam, method = "ward.D")
+plot(clust.sam)
+groups.sam <- cutree(clust.sam, 6)
+rect.hclust(clust.sam, k=6, border="red")
+table(groups.sam)
+#  1   2   3   4   5   6 
+#103  47 123  79  69 101 
+groups.gender <- cbind(group=groups.sam, gender=g1k.mf$gender)
+plot(gender ~ group, groups.gender)
+
+clust.sam1 <- hclust(g1k.dist.sam) #method =complete
+plot(clust.sam1)
+groups.sam1 <- cutree(clust.sam1, 6)
+rect.hclust(clust.sam1, k=6, border="red")
+table(groups.sam1)
+#  1   2   3   4   5   6 
+# 52  49 103  55 145 118 
+groups.gender1 <- cbind(group=groups.sam1, gender=g1k.mf$gender)
+plot(gender ~ group, groups.gender1)
+
+#The clustering does not show differences between M and F gene expression with both the methods
+
+#Lets look at the gene clustering
+g1k.mf.t <- t(g1k.mf.m) #Transpose the matrix
+g1k.dist <- dist(g1k.mf.t)
+
+clust.gene <- hclust(g1k.dist, method = "ward.D")
+plot(clust.gene)
+groups.gene <- cutree(clust.gene, 4)
+gene.cluster <- rect.hclust(clust.gene, k=4, border="red")
+sapply(gene.cluster, length) #The actual order in the dendrogram
+# 134 259 400 207
+table(groups.gene)
+#   1   2   3   4 
+# 134 400 207 259 
+
+clust.gene1 <- hclust(g1k.dist, method = "complete")
+plot(clust.gene1)
+groups.gene1 <- cutree(clust.gene1, 5)
+gene.cluster1 <- rect.hclust(clust.gene1, k=5, border="red")
+sapply(gene.cluster1, length) #The actual order in the dendrogram
+# 13 522 238 133  94
+table(groups.gene1)
+#   1   2   3   4   5 
+# 133  94  13 238 522 
+
+library(RColorBrewer)
+display.brewer.all() # To see all availabel colors
+niceCols <- brewer.pal(9, "Set1")
+
+#Plot the distribution for the clustered genes from both the sets
+#Matplot plots the columns of one matrix against columns of another
+oPar <- par(mfrow=c(2,2))
+for (i in 1:4) {
+  matplot(t(g1k.mf.t[groups.gene == i,]),
+          type="p", col=niceCols[i], 
+          xlab="sample",ylab="log expression value")
+}
+par(oPar)
+
+oPar <- par(mfrow=c(3,2))
+for (i in 1:5) {
+  matplot(t(g1k.mf.t[groups.gene1 == i,]),
+          type="l", col=niceCols[i], 
+          xlab="sample",ylab="log expression value")
+}
+par(oPar)
+
+#Does the group 1 from both clustering methods have the same genes?
+match.gene <- (colnames(g1k.mf.m[ ,groups.gene == 1]) %in% colnames(g1k.mf.m[ ,groups.gene1 == 1]))
+which(match.gene == FALSE)
+#All the genes match except #130 that is extra in groups.gene
+colnames(g1k.mf.m[ ,groups.gene == 1])[130] #X6444
+
